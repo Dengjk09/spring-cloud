9a1247cd6d4b45587846b3516e7222d7c34d950d
dengjk <277991520@qq.com>
2018-07-09 23:53:13 +0800

    电影微服务提交

---
+++ cloud-movie-consumer/src/main/java/com/dengjk/cloudmovieconsumer/CloudMovieConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.dengjk.cloudmovieconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudMovieConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudMovieConsumerApplication.class, args);
+	}
+}
