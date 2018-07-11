763594a66435758b056a9110535c9f948dcfa81f
djk <277991520@qq.com>
2018-07-11 14:28:58 +0800

    feign实现

--- cloud-movie-consumer/src/main/java/com/dengjk/cloudmovieconsumer/CloudMovieConsumerApplication.java
+++ cloud-movie-consumer/src/main/java/com/dengjk/cloudmovieconsumer/CloudMovieConsumerApplication.java
@@ -1,15 +1,35 @@
 package com.dengjk.cloudmovieconsumer;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class CloudMovieConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudMovieConsumerApplication.class, args);
+	}
+
+	@Bean
+	public RequestInterceptor headerInterceptor() {
+		return new RequestInterceptor() {
+			@Override
+			public void apply(RequestTemplate requestTemplate) {
+				requestTemplate.header("Cookie", "703EDE2DCE92A16E05669D61055C7190");
+			}
+		};
 	}
 }
--- cloud-movie-consumer/src/main/java/com/dengjk/cloudmovieconsumer/controller/MovieController.java
+++ cloud-movie-consumer/src/main/java/com/dengjk/cloudmovieconsumer/controller/MovieController.java
@@ -1,7 +1,10 @@
 package com.dengjk.cloudmovieconsumer.controller;
 
 import com.dengjk.cloudmovieconsumer.entity.UserEntity;
+import com.dengjk.cloudmovieconsumer.service.ExpressFeignReq;
+import com.dengjk.cloudmovieconsumer.service.UserFeignReq;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -19,9 +22,27 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UserFeignReq userFeignReq;
+
+    @Autowired
+    private ExpressFeignReq expressFeignReq;
+
     @GetMapping("getUser/{id}")
+    @ApiOperation("使用ribbon负载调用获取用户信息接口")
     public UserEntity getUserById(@PathVariable(value = "id") Integer id) {
         UserEntity userEntity = restTemplate.getForObject("http://cloud-user-provid/user/getUser/" + id, UserEntity.class);
         return userEntity;
+    }
+
+    @GetMapping("/getUserByFeign/{id}")
+    @ApiOperation("使用申明式feign获取用户信息")
+    public UserEntity getUserByIdUFeign(@PathVariable(value = "id") Integer id){
+       return userFeignReq.getUserById(id);
+    }
+
+    @GetMapping("/getExpressInfo")
+    public String getExpressInfo(){
+        return expressFeignReq.getExpress();
     }
 }
