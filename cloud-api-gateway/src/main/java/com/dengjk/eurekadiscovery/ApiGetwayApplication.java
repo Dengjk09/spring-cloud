3aa8ecf40ff33d5e0075daa7846795ac41e9c4e1
djk <277991520@qq.com>
2018-07-16 20:51:40 +0800

    加入apigateway网关

---
+++ cloud-api-gateway/src/main/java/com/dengjk/eurekadiscovery/ApiGetwayApplication.java
@@ -0,0 +1,22 @@
+package com.dengjk.eurekadiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Dengjk
+ * @create 2018-06-29 16:02
+ * @desc  spring-cloud   api统一网关
+ *
+ *	把所有的微服务给代理起来,提供一个入口 类似于nginx的的反向代理
+ *
+ **/
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetwayApplication.class, args);
+	}
+}
