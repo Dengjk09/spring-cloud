8e98abb717d17244e96b6155e9b321ecc7c7ce8d
djk <277991520@qq.com>
2018-07-18 22:42:49 +0800

    thymeleaf模板加入

--- daily-study/src/main/java/com/dengjk/dailystudy/controller/ThymeleafController.java
+++ daily-study/src/main/java/com/dengjk/dailystudy/controller/ThymeleafController.java
@@ -2,18 +2,23 @@ package com.dengjk.dailystudy.controller;
 
 import com.dengjk.dailystudy.dao.UserRepository;
 import com.dengjk.dailystudy.entity.UserEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@Api(value = "thymeleaf模板",description = "thymeleaf模板测试")
 public class ThymeleafController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/user")
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @ApiOperation(value = "跳转到首页")
     public String forwardFirstIndex(ModelMap modelMap){
         UserEntity user = userRepository.findOne(1);
         modelMap.addAttribute("user", user);
