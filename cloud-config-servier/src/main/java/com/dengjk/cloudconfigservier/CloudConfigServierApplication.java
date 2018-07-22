e5db2e0ad9b3ad0d9d185fa8b24323267ed2f600
dengjk <277991520@qq.com>
2018-07-22 21:49:44 +0800

    springcloud-config配置

---
+++ cloud-config-servier/src/main/java/com/dengjk/cloudconfigservier/CloudConfigServierApplication.java
@@ -0,0 +1,17 @@
+package com.dengjk.cloudconfigservier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Dengjk
+ * @create 2018-06-29 16:02
+ * @desc spring-cloud-config-service  服务
+ *
+ **/
+@SpringBootApplication
+public class CloudConfigServierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServierApplication.class, args);
+	}
+}
