4f10327f5f7e2622bdef21c84280a9c5b4ffb98c
dengjk <277991520@qq.com>
2018-07-23 23:41:35 +0800

    config-server嵌入eureka

--- cloud-config-client/src/main/java/com/dengjk/cloudconfigclient/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/dengjk/cloudconfigclient/CloudConfigClientApplication.java
@@ -2,6 +2,9 @@ package com.dengjk.cloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @author Dengjk
  * @create 2018-06-29 16:02
@@ -14,6 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudConfigClientApplication {
 
 	public static void main(String[] args) {
--- cloud-config-servier/src/main/java/com/dengjk/cloudconfigservier/CloudConfigServierApplication.java
+++ cloud-config-servier/src/main/java/com/dengjk/cloudconfigservier/CloudConfigServierApplication.java
@@ -3,6 +3,7 @@ package com.dengjk.cloudconfigservier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Dengjk
@@ -12,6 +13,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  **/
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class CloudConfigServierApplication {
 
 	public static void main(String[] args) {
