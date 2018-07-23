35387ea76ab9cf892d88e79c4ce486af5830ed1e
dengjk <277991520@qq.com>
2018-07-23 23:55:35 +0800

    config-server嵌入eureka

--- cloud-config-client/src/main/java/com/dengjk/cloudconfigclient/controller/ReadProfileInfoController.java
+++ cloud-config-client/src/main/java/com/dengjk/cloudconfigclient/controller/ReadProfileInfoController.java
@@ -1,6 +1,7 @@
 package com.dengjk.cloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Dengjk
  * @create 2018-06-29 16:02
  * @desc   编写一个controller来模拟读取config-server上在git上面的配置文件信息
- *
+ *          @RefreshScope 该注解表明这里是一个可自动刷新 重新加载配置文件
  **/
 @RestController
+@RefreshScope
 public class ReadProfileInfoController {
 
     @Value("${profile}")
