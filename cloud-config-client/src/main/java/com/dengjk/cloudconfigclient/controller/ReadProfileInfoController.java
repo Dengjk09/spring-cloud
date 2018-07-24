415facd012aac83c9989f43d04f0a41a63dfe4d8
dengjk <277991520@qq.com>
2018-07-24 23:07:48 +0800

    config-server嵌入eureka

--- cloud-config-client/src/main/java/com/dengjk/cloudconfigclient/controller/ReadProfileInfoController.java
+++ cloud-config-client/src/main/java/com/dengjk/cloudconfigclient/controller/ReadProfileInfoController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @desc 编写一个controller来模拟读取config-server上在git上面的配置文件信息
  * @RefreshScope 该注解表明这里是一个可自动刷新 重新加载配置文件
  * 站点来访问  http://localhost:9097/refresh  重新加载git上面的配置文件
+ *
+ *  也可以通过springcloud-bus 来刷新  ,嵌入rabbitmq 做为消息推送,更新所有的config-server下面的所有client
+ *
+ *  导入bus-amqp的依赖
+ *
  */
 @RestController
 @RefreshScope
