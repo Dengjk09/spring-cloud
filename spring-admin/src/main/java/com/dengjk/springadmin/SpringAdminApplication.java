7def1390e06c9664401310037439b2192de3f07e
djk <277991520@qq.com>
2019-11-19 16:08:39 +0800

    springboot-admin

---
+++ spring-admin/src/main/java/com/dengjk/springadmin/SpringAdminApplication.java
@@ -0,0 +1,20 @@
+package com.dengjk.springadmin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot-admin  2.0一下需要依赖ui的包
+ */
+
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminApplication.class, args);
+    }
+
+}
